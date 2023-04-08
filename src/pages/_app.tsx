80e6d208630f7fc7e0a0e88e5665eab22fc13b45
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-08 22:30:55 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import localFont from 'next/font/local'
+import Head from 'next/head'
 
 const poppins = localFont({
   src: [
@@ -21,6 +22,16 @@ const poppins = localFont({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+    <Script id="google-tag-manager" strategy='afterInteractive'>
+          {`
+          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TG6SCN8');
+          `}
+        </Script>
+    </Head>
     <div className={poppins.className}>
       <Script src="/js/app.js" />
       <Script src="/js/fontAwesome.js" />
