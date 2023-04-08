832de090d05aecbd6342c9d6f3920ba5cba81125
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-04-08 22:26:32 +0200

    Update meta.tsx

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Meta() {
   return (
@@ -12,6 +13,14 @@ export default function Meta() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <meta charSet="utf-8" />
+        <Script id="google-tag-manager" strategy='afterInteractive'>
+          {`
+          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TG6SCN8');
+          `}
+        </Script>
       </Head>
     </>
   )
